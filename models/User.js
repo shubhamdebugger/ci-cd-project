0ddbde0dfe4de5393726b2431f765af8c141aaea
Shubhamprogrammar <mauryashubham12349@gmail.com>
2026-04-18 20:32:12 +0530

    models optimised

--- models/User.js
+++ models/User.js
@@ -1,11 +1,27 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  first_name: String,
-  last_name: String,
-  email: String,
-  phone: String,
-  password: String,
+  first_name: {
+    type: String,
+    required: true,
+  },
+  last_name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 },{timestamps:true});
 
 module.exports = mongoose.model("user", userSchema);
